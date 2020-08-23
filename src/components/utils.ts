88cffe81c10ce52e21bcd8b5a17178649fb617aa
Cuong Tran <103cuong@gmail.com>
2020-08-23 21:42:44 +0700

    Initial commit

---
+++ src/components/utils.ts
@@ -0,0 +1,19 @@
+import Umzug from 'umzug';
+import { Sequelize } from 'sequelize';
+
+export const migrateDB = (sequelize: Sequelize, path: string) => new Umzug({
+  migrations: {
+    path,
+    pattern: /\.migration.[t|j]s$/,
+    params: [
+      sequelize.getQueryInterface(),
+      sequelize.constructor,
+      () => {
+        throw new Error(`Migration tried to use old style "done" callback.
+          Please upgrade to "umzug" and return a promise instead.`);
+      },
+    ],
+  },
+  storage: 'sequelize',
+  storageOptions: { sequelize },
+}).up();
