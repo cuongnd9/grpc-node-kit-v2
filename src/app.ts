27875d2c9aebdd1731587744e66b97fbd62bf6a2
Cuong Tran <103cuong@gmail.com>
2020-09-01 17:42:21 +0700

    chore: grpc error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import Mali from 'mali';
+import Mali, { Context } from 'mali';
+import { logger } from 'juno-js';
 
 import { createGRPCServices } from './grpc';
 
 const createApp = () => {
   const app = new Mali();
   createGRPCServices(app);
+  app.on('error', (err: Error, ctx: Context) => {
+    logger.error(`😥 Server error for call ${ctx.name} of type ${ctx.type}`, err);
+  });
   return app;
 };
 
